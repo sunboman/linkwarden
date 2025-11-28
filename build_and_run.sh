5a1265244151e4c2bfd56736bcc0e456e2289b0e
ugst <sunboman@gmail.com>
2025-11-28 01:09:29 -0800

    chore: Update deployment script and remove MeiliSearch

--- build_and_run.sh
+++ build_and_run.sh
@@ -21,6 +21,23 @@ if [ "$BUILD" = true ]; then
   docker compose build
 fi
 
+# Generate .env if missing
+if [ ! -f .env ]; then
+  echo "Generating .env file..."
+  POSTGRES_PASSWORD=$(openssl rand -hex 16)
+  NEXTAUTH_SECRET=$(openssl rand -base64 32)
+  
+  cat <<EOF > .env
+NEXTAUTH_URL=http://localhost:$PORT/api/v1/auth
+NEXTAUTH_SECRET=$NEXTAUTH_SECRET
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+DATABASE_URL=postgresql://postgres:\${POSTGRES_PASSWORD}@postgres:5432/postgres
+EOF
+  echo ".env file generated."
+else
+  echo ".env file exists. Skipping generation."
+fi
+
 echo "Starting containers on port $PORT..."
 docker compose up -d
 
