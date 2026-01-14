e1546d1202794b289c4410afc39b3c7f5ee35085
ugst <sunboman@gmail.com>
2026-01-14 00:45:54 -0800

    Make archiveOrgProxyDomains migration idempotent

--- packages/prisma/migrations/20260114000000_add_archive_org_proxy_domains/migration.sql
+++ packages/prisma/migrations/20260114000000_add_archive_org_proxy_domains/migration.sql
@@ -1,2 +1,10 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "archiveOrgProxyDomains" TEXT[] DEFAULT ARRAY[]::TEXT[];
+-- AlterTable (idempotent - column may already exist from db push)
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'User' AND column_name = 'archiveOrgProxyDomains'
+    ) THEN
+        ALTER TABLE "User" ADD COLUMN "archiveOrgProxyDomains" TEXT[] DEFAULT ARRAY[]::TEXT[];
+    END IF;
+END $$;
