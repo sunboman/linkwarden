cd27b5ce840e3b99809b57f12b059043fad2630c
ugst <sunboman@gmail.com>
2026-01-15 22:24:24 -0800

    fix: remove unused align prop from ToggleDarkMode usage

--- apps/web/pages/public/collections/[id]/index.tsx
+++ apps/web/pages/public/collections/[id]/index.tsx
@@ -208,7 +208,7 @@ export default function PublicCollections() {
                 </Tooltip>
               </TooltipProvider>
 
-              <ToggleDarkMode align="left" />
+              <ToggleDarkMode />
 
               <TooltipProvider>
                 <Tooltip>
