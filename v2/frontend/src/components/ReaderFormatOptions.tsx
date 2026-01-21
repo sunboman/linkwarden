c67ab26e9c81f73c72af8daf8855ec4582744208
ugst <sunboman@gmail.com>
2026-01-21 13:59:12 -0800

    Fix syntax error: remove stray closing div

--- v2/frontend/src/components/ReaderFormatOptions.tsx
+++ v2/frontend/src/components/ReaderFormatOptions.tsx
@@ -138,7 +138,7 @@ export function ReaderFormatOptions({ onSettingsChange, currentSettings }: Reade
                  </div>
               </div>
 
-              </div>
+
 
               {/* Theme Row */}
               <div className="flex items-center justify-between px-3 py-2">
